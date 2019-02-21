5935a90a5a856b4c750341df75780a81d8fe100b
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-02-21 13:25:56 -0800

    String Methods

--- src/charAt_scanners_optionDialogs/StringMethods1.java
+++ src/charAt_scanners_optionDialogs/StringMethods1.java
@@ -1,4 +1,7 @@
 package charAt_scanners_optionDialogs;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -8,22 +11,31 @@ public class StringMethods1 {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-
+String sandwhich = JOptionPane.showInputDialog("do you want a sandwhich");
+		for (int i = 0; i < sandwhich.length();i++) {
+			
+		
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+		//	System.out.println(sandwhich.charAt(2));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+		System.out.println(sandwhich.charAt(i));
+		
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+		}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
+		for (int i = 0; i < sandwhich.length();i++) {
+			if(sandwhich.charAt(i)=='y') {
+				System.out.println(i);
+			}
+		}
 		
 	}
 }
