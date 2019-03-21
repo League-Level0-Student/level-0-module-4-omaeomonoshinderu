45fbef8ad072e536ecf68c4fb2957d84932c75df
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-03-21 13:32:53 -0700

    Own Adventure

---
+++ src/extra/OwnAdventure.java
@@ -0,0 +1,20 @@
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class OwnAdventure {
+public static void main(String[] args) {
+	String pretty Penny = JOptionPane.showOptionDialog("You see a pretty penny","happy pet", 0, JOptionPane.INFORMATION_MESSAGE, null);
+			new String[] { "leave it", "Pick it up" }, null);
+	//(pick up penny and fall in sewer
+	String sewer =JOptionPane.showOptionDialog(null, "You fell down the sewer what will you do?", "happy pet", 0, JOptionPane.INFORMATION_MESSAGE, null);
+	//scream for help get eaten
+	//try to get out break your neck)
+	
+	
+	//(leave penny wonder
+	//go back fall in sewer
+	//leave penny the end)
+	
+}
+}
